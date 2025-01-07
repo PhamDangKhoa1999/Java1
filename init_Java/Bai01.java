b74d44532202be91205c4e0edcd3f15f72322a03
PhamDangKhoa1999 <khoaphamdang1999@gmail.com>
2025-01-08 00:22:17 +0700

    Code Java Lab01

---
+++ init_Java/Bai01.java
@@ -0,0 +1,29 @@
+package init_Java;
+
+import java.util.Scanner;
+
+public class Bai01 {
+    public static void main(String[] ars)
+    {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bai 1: Nhap tu ban phim mot ki tu bat ky: ");
+        String a = scanner.nextLine();
+        System.out.println("Ki tu ban vua nhap tu ban phim la: " + a);
+
+        System.out.println("Bai 2:");
+        System.out.println("Nhap chieu dai hinh chu nhat: ");
+        int chieuDai = scanner.nextInt();
+        System.out.println("Nhap chieu rong hinh chu nhat: ");
+        int chieuRong = scanner.nextInt();
+        int P = (chieuDai + chieuRong) * 2;
+        System.out.println("Chu vi hinh chu nhat la: " + P );
+        int S = chieuDai * chieuRong;
+        System.out.println("Dien tich hinh chu nhat la: " + S);
+        System.out.println("Canh nho nhat cua hinh chu nhat la: " + Math.min(chieuDai, chieuRong));
+
+        System.out.println("Bai 3:");
+        System.out.println("Nhap mot canh cua hinh lap phuong: ");
+        int x = scanner.nextInt();
+        System.out.println("The tich hinh lap phuong la: " + Math.pow(x, 3));
+    }
+}
--- init_Java/HelloWorld.java
+++ init_Java/HelloWorld.java
@@ -1,11 +1,27 @@
 package init_Java;
 
+import java.util.Scanner;
+
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello Galaxy");
-        System.out.println("Hello khoa.pham");
-        System.out.println("Hello samsung");
+        int test1 = 10 + 2;
+        System.out.println("Result:  " + test1);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Your age: ");
+        int age = scanner.nextInt();
+        System.out.println("Your name: " + name + ", your age: "  + age);
+
+        int a = 10;
+        int b = 6;
+        int c = Math.max(a, b);
+        System.out.println("firtNumber: ");
+        int firtNumber = scanner.nextInt();
+        System.out.println("secondNumber: ");
+        int secondNumber = scanner.nextInt();
+        System.out.println("Min = " +  Math.min(firtNumber, secondNumber));
     }
 }
