ec61754ea46c774d146d1dd31439854eadc94dde
PhamDangKhoa1999 <khoaphamdang1999@gmail.com>
2025-01-07 22:48:27 +0700

    add content

---
+++ init_Java/HelloWorld.java
@@ -0,0 +1,11 @@
+package init_Java;
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Hello Galaxy");
+        System.out.println("Hello khoa.pham");
+        System.out.println("Hello samsung");
+    }
+}
