6847c5b1358e18d25c332b5926f5b3e8d821acf2
PhamDangKhoa1999 <khoaphamdang1999@gmail.com>
2025-01-12 23:44:07 +0700

    Array

---
+++ init_Java/Lab03.java
@@ -0,0 +1,13 @@
+package init_Java;
+
+public class Lab03 {
+     public static void main(String[] args) {
+        String[] doiBongToiYeu = new String[3];
+        doiBongToiYeu[0] = "Barca";
+        doiBongToiYeu[1] = "MU";
+        doiBongToiYeu[2] = "MC";
+        for (int i = 0; i < doiBongToiYeu.length; i++){
+            System.out.println("Doi bong toi yeu " + i + ":" + doiBongToiYeu[i]);
+        }
+     }
+}
